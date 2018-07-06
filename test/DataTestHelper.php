7f22c6196afc113f709bb0024aee57c338264c4d
Tint Naing Win <me@tintnaingwin.com>
2018-07-06 21:37:07 +0630

    Added Phpunit

--- test/DataTestHelper.php
+++ test/DataTestHelper.php
@@ -4,12 +4,12 @@ namespace test;
 
 trait DataTestHelper
 {
-    protected function zawgyiData(): string
+    protected function zawgyiData()
     {
         return 'သီဟိုဠ္မွ ဉာဏ္ႀကီးရွင္သည္ အာယုဝၯနေဆးၫႊန္းစာကို ဇလြန္ေဈးေဘး ဗာဒံပင္ထက္ အဓိ႒ာန္လ်က္ ဂဃနဏဖတ္ခဲ့သည္။';
     }
 
-    protected function unicodeData(): string
+    protected function unicodeData()
     {
         return 'သီဟိုဠ်မှ ဉာဏ်ကြီးရှင်သည် အာယုဝဍ္ဎနဆေးညွှန်းစာကို ဇလွန်ဈေးဘေး ဗာဒံပင်ထက် အဓိဋ္ဌာန်လျက် ဂဃနဏဖတ်ခဲ့သည်။';
     }
--- test/MyanFontTest.php
+++ test/MyanFontTest.php
@@ -13,15 +13,28 @@ class MyanFontTest extends \PHPUnit\Framework\TestCase
         UNICODE = 'Unicode',
         MYANMAR = 'Myanmar';
 
+    /**
+     * @throws \Exception
+     *
+     * @author Tint Naing Win <me@tintnainwin.com>
+     */
     public function test_check_font()
     {
         $font = MyanFont::checkFont($this->zawgyiData());
+        print ($font);
+
         $this->assertEquals($font, self::ZAWGYI);
 
         $font = MyanFont::checkFont($this->unicodeData());
+        print ($font);
         $this->assertEquals($font, self::UNICODE);
     }
 
+    /**
+     * @throws \Exception
+     *
+     * @author Tint Naing Win <me@tintnainwin.com>
+     */
     public function test_convert_zg2uni()
     {
         $convert = MyanFont::zg2uni($this->zawgyiData());
@@ -29,6 +42,11 @@ class MyanFontTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals($font, self::UNICODE);
     }
 
+    /**
+     * @throws \Exception
+     *
+     * @author Tint Naing Win <me@tintnainwin.com>
+     */
     public function test_convert_uni2zg()
     {
         $convert = MyanFont::uni2zg($this->unicodeData());
